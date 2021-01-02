d148917b2a1dca1db598311ad557ba3c8b815452
Peter Storm <peter.storm@peterstorm.io>
2021-01-02 14:51:40 +0100

    More changes to folder structure

--- todo/src/main/scala/delivery/AssetService.scala
+++
@@ -1,9 +0,0 @@
-package delivery
-
-import cats.effect._
-import org.http4s.HttpRoutes
-import org.http4s.server.staticcontent._
-
-object AssetService:
-    def service[F[_]: Sync](blocker: Blocker)(using cs: ContextShift[F]): HttpRoutes[F] =
-      fileService(FileService.Config("./assets", blocker))
